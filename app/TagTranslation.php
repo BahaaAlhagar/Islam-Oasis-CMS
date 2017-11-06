a83d20dfdf21e9b329eac8aaa8b13a2e525286c9
BahaaAlhagar <damrawyy@yahoo.com>
2017-11-06 20:43:25 +0200

    tag translation modal - migration

---
+++ app/TagTranslation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Database\Eloquent\Model;
+
+class TagTranslation extends Model
+{
+    public $timestamps = false;
+    protected $guarded = ['id'];
+
+    use HasSlug;
+    
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+}
---
+++ database/migrations/2017_11_06_203905_create_tag_translations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_translations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tag_id')->unsigned()->index();
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->string('locale')->index();
+            $table->string('name');
+            $table->string('slug')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_translations');
+    }
+}
