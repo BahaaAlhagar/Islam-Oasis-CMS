120170b518ddc64182adaf7f15d2047345c6be6b
BahaaAlhagar <damrawyy@yahoo.com>
2017-12-04 21:08:13 +0200

    news index page content

---
+++ resources/lang/ar/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'more'              => 'اقرأ المزيد',
+    'archives'          => 'الارشيف',
+    'title'             => 'واحة الاسلام',
+    'description'       => 'اقرأ عن الاسلام',
+];
--- resources/views/FrontEnd/news/index.blade.php
+++ resources/views/FrontEnd/news/index.blade.php
@@ -1,6 +1,28 @@
 @extends('FrontEnd/layouts/default')
 
 
-news index
-@include('FrontEnd/news/_sidebar')
+@section('content')
 
+
+			@foreach($news as $post)
+				<div class="blog-post">
+		            <h2 class="blog-post-title">
+		            	<a href="{{ route('news.show', $post->slug) }}">
+		            		{{ $post->title }}
+		            	</a>
+		            </h2>
+		            <p class="blog-post-meta">{{ $post->created_at->diffForHumans() }}</p>
+
+		            {!! str_limit($post->content, 200) !!}
+
+		            <a href="{{ route('news.show', $post->slug) }}">{{ __('messages.more')}}</a>
+		          </div>
+			@endforeach
+
+			{{ $news->links() }}
+
+@endsection
+
+@section('sidebar')
+	@include('FrontEnd/news/_sidebar')
+@endsection
