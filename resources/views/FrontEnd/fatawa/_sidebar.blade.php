c0dfa1c2259af2c871300e9d8f5dd284b4b57b96
BahaaAlhagar <damrawyy@yahoo.com>
2017-12-09 13:51:22 +0200

    fatawa sidebar

---
+++ resources/views/FrontEnd/fatawa/_sidebar.blade.php
@@ -0,0 +1,20 @@
+
+		<div class="sidebar-module">
+			<h4>{{ __('messages.also_like') }}</h4>
+			<ol class="list-unstyled">
+				@foreach($randomFatawa as $fatwa)
+					<li><a href="{{ route('fatawa.show', $fatwa->slug) }}">{{ $fatwa->question }}</a></li>
+				@endforeach
+			</ol>
+		</div>
+
+		@if(count($tags))
+			<div class="sidebar-module">
+			    <h4>{{ __('messages.categories') }}</h4>
+			    <span class="tags-list">
+			    	@foreach($tags as $tag)
+			    		<li><a href="{{ route('fatawaTag.show', $tag->slug) }}">{{ $tag->translate(app()->getLocale())->name }} <button type="button" class="btn btn-warning btn-round-xs btn-xs"> {{ $tag->fatawa_count }}</button></a></li>
+					@endforeach
+				</span>
+		  	</div>
+		@endif
